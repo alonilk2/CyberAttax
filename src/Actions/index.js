3bd049dd97557645bbc51b75961fd622fd6a5b66
alonilk2 <63236463+alonilk2@users.noreply.github.com>
2022-03-23 18:05:57 +0200

    1

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-import { DATA_FETCHED, DATA_FETCH_ERROR, INCREMENT } from '../Constants'
+import { DATA_FETCHED, DATA_FETCH_ERROR, INCREMENT, SERVER_URL } from '../Constants'
 /**
  * An action function which sends a GET request to fetch attack data from the server.
  * After successful response, the data is then uploaded to a global state variables (using Redux's dispatch)
@@ -7,7 +7,7 @@ import { DATA_FETCHED, DATA_FETCH_ERROR, INCREMENT } from '../Constants'
  */
 const FetchDataFromDB = () => async dispatch => {
   try {
-    const response = await Axios.get('http://localhost:5000/fetchdata')
+    const response = await Axios.get(SERVER_URL+'/fetchdata')
     if (response.data) dispatch({ type: DATA_FETCHED, payload: response.data })
   } catch (error) {
     dispatch({ type: DATA_FETCH_ERROR, payload: error })
--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,3 +1,4 @@
 export const DATA_FETCHED = 'DATA_FETCHED'
 export const DATA_FETCH_ERROR = 'DATA_FETCHED_ERROR'
 export const FILTER_RESULTS = 'FILTER_RESULTS'
+export const SERVER_URL = 'https://cyberattax.herokuapp.com'
